14c0c0eebcf3b50add5c71463cb0fee9ebc908a0
FABIO-PC\fabio <fabioagilda@gmail.com>
2022-01-18 17:18:39 +0100

    Async Await

---
+++ playground/promise-chaining-2.js
@@ -0,0 +1,15 @@
+require('../src/db/mongoose')
+
+const Task = require('../src/models/task')
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('61e07060afebecdc1ed82548').then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
+})
--- src/index.js
+++ src/index.js
@@ -8,25 +8,76 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users',(req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-app.post('/tasks', (req, res) => {
+// Fetch all users
+app.get('/users', async (req,res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Fetch single user
+app.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById(_id)
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body) // Utiliza los datos del body
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
+
+    try {
+       await task.save() 
+       res.status(201).send(task)
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
+})
+
+// Fetch all Tasks
+
+app.get('/tasks', async (req, res) =>{
+
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+app.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+       const task = await Task.findById(_id)
+        res.send(task)  
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}`)
+    console.log('Server is up on port', port)
 })
