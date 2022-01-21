498e963cecd06e01369bed4fd2661eb04373fb26
FABIO-PC\fabio <fabioagilda@gmail.com>
2022-01-21 19:02:22 +0100

    sesion tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,13 +5,23 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-
+    const token = await user.generateAuthToken()
     try {
         await user.save()
-        res.status(201).send(user)
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken() // Another name is valid
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }    
 })
 
 // Fetch all users
@@ -49,7 +59,13 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        //Middleware
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save()
 
         if (!user) {
             return res.status(404).send()
