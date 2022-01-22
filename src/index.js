d4051ffa1e2e220f79a90937a0219cb7c69f3759
FABIO-PC\fabio <fabioagilda@gmail.com>
2022-01-22 15:32:22 +0100

    user-task relations

--- src/index.js
+++ src/index.js
@@ -18,8 +18,21 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-
-
 app.listen(port, () => {
     console.log('Server is up on port', port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () =>  {
+    // const task = await Task.findById('61ebfc81e1a8430b26787583')
+    // await task.populate('owner')
+    // console.log(task.owner)
+
+    const user = await User.findById('61ebfb0a072ca9cc526f2fce')
+    await user.populate('tasks')
+    console.log(user.tasks)
+}
+
+main()
