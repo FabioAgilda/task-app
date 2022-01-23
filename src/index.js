c86832eb5056fd901dc3ddaed268f578732f6c2a
FABIO-PC\fabio <fabioagilda@gmail.com>
2022-01-23 16:43:06 +0100

    sort & pagination

--- src/index.js
+++ src/index.js
@@ -6,14 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('get requests are disable')
-//     }else{
-//         next()
-//     }
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -21,18 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port', port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () =>  {
-    // const task = await Task.findById('61ebfc81e1a8430b26787583')
-    // await task.populate('owner')
-    // console.log(task.owner)
-
-    const user = await User.findById('61ebfb0a072ca9cc526f2fce')
-    await user.populate('tasks')
-    console.log(user.tasks)
-}
-
-main()
--- src/routers/task.js
+++ src/routers/task.js
@@ -17,13 +17,33 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-// Fetch all Tasks
-
+// GET /tasks?completed=false
+// GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt_desc
 router.get('/tasks', auth, async (req, res) =>{
+    const match = {}
+    const sort = {}
+
+    if (req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
 
     try {
         // const task = await Task.find({owner: req.user._id})
-        await req.user.populate('tasks')
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        })
 
         res.send(req.user.tasks)
     } catch (e) {
